30c3d6aa0f50ba0d488b2b36c6e6a5f4647a2c55
ayushkumar0208 <aryaayushgithub0208@gmail.com>
2023-07-13 16:42:50 +0530

    grf

---
+++ front-end/build/precache-manifest.36a18759255858d332eb19084431f994.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ae7e75deb71a280ed4238916a53ba6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2420cdde70e3b7c44c0e",
+    "url": "/static/css/2.57ba64fa.chunk.css"
+  },
+  {
+    "revision": "576db49114b76b9fb053",
+    "url": "/static/css/main.931d5d5d.chunk.css"
+  },
+  {
+    "revision": "2420cdde70e3b7c44c0e",
+    "url": "/static/js/2.a9afc2a3.chunk.js"
+  },
+  {
+    "revision": "6aed8591a7f03c4b37412030ac84d260",
+    "url": "/static/js/2.a9afc2a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "576db49114b76b9fb053",
+    "url": "/static/js/main.92c8989f.chunk.js"
+  },
+  {
+    "revision": "5ff6df50c1a535018958",
+    "url": "/static/js/runtime-main.df338948.js"
+  }
+]);
